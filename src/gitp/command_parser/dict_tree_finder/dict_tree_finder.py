7ceef067533dd85f6233306298bcea0f1bd81766
DarkTrick <notebook22312@gmail.com>
2022-09-18 18:22:36 +0900

    Refactor: DictTreeFinder: clearity

--- src/gitp/command_parser/dict_tree_finder/dict_tree_finder.py
+++ src/gitp/command_parser/dict_tree_finder/dict_tree_finder.py
@@ -4,7 +4,7 @@ from typing import List, Dict
 
 class DictTreeFindResult:
   def __init__(self):
-    self.branch: Dict | None = {}
+    self.branch: Dict | None = None
     self.variableDictionary: Dict = {}
 
     """
@@ -48,7 +48,6 @@ def _dictTree_find(dictTree: Dict, searchPath: List[str], result: DictTreeFindRe
     newSearchPath = searchPath[1:]
 
     if(not isinstance(newTree, dict)):
-      result.branch = None
       return result
 
     return _dictTree_find(newTree, newSearchPath, result)
@@ -58,11 +57,9 @@ def _dictTree_find(dictTree: Dict, searchPath: List[str], result: DictTreeFindRe
   for branchKey, branch in dictTree.items():
     if(_isVariable(branchKey)):
       result.variableDictionary[branchKey] = searchKey
-      result.branch = branch
       newTree = dictTree[branchKey]
       newSearchPath = searchPath[1:]
       return _dictTree_find(newTree, newSearchPath, result, True)
 
-  # no match found, so return None
-  result.branch = None
+  # no match found
   return result
