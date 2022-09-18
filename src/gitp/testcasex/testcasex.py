7bed690e3542ad7ddd6d34aade97801c376709e6
DarkTrick <notebook22312@gmail.com>
2022-09-18 17:35:57 +0900

    TestCaseX: Improved test output

--- src/gitp/testcasex/testcasex.py
+++ src/gitp/testcasex/testcasex.py
@@ -8,16 +8,30 @@ class _ActualTestValue:
   """
   def __init__(self, testcase: unittest.TestCase):
     self.actualValue = None
+    self.expectedValue = None
     self.testcase = testcase
 
+  def _formatMessage(self, msg):
+
+    _msg = ""
+    if(None != msg):
+      _msg = "\n" + "   Message:  " + str(msg)
+
+    return "\n" + "   Expected: " + str(self.expectedValue) + \
+           "\n" + "   Actual:   " + str(self.actualValue)   + \
+            _msg
+
   def toBe(self, expectedValue, msg = None):
-    self.testcase.assertEqual (self.actualValue, expectedValue, msg)
+    self.expectedValue = expectedValue
+    self.testcase.assertEqual (self.actualValue, expectedValue, self._formatMessage(msg))
 
   def toNotBe(self, expectedValue, msg = None):
-    self.testcase.assertFalse (self.actualValue == expectedValue, msg)
+    self.expectedValue = expectedValue
+    self.testcase.assertFalse (self.actualValue == expectedValue, self._formatMessage(msg))
 
   def toContain(self, expectedValue, msg = None):
-    self.testcase.assertTrue (expectedValue in self.actualValue, msg)
+    self.expectedValue = expectedValue
+    self.testcase.assertTrue (expectedValue in self.actualValue, self._formatMessage(msg))
 
 
 class TestCaseX(unittest.TestCase):
